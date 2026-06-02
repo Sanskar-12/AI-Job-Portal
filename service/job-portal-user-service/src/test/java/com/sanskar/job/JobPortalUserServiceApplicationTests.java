aad85b4059ca34cee780d7c3ea94aa1a8f17679f
Sanskar-12 <sanskarv2004@gmail.com>
2026-06-02 18:04:08 +0530

    Feature: Connected postgres db in user service

---
+++ service/job-portal-user-service/src/test/java/com/sanskar/job/JobPortalUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanskar.job;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobPortalUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
