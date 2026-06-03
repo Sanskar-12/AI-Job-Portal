9d06e33aacfa1c1d3c09def6240d96a4c70ef08d
Sanskar-12 <sanskarv2004@gmail.com>
2026-06-03 17:33:59 +0530

    Feature: Build whole app and used one project dependency into another project

---
+++ common-lib/src/main/java/com/sanskar/job/domain/UserRole.java
@@ -0,0 +1,5 @@
+package com.sanskar.job.domain;
+
+public enum UserRole {
+    ROLE_ADMIN, ROLE_JOB_SEEKER, ROLE_EMPLOYER
+}
--- service/job-portal-user-service/src/main/java/com/sanskar/job/controller/HomeController.java
+++ service/job-portal-user-service/src/main/java/com/sanskar/job/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.sanskar.job.controller;
 
+import com.sanskar.job.domain.UserRole;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping
     public String HomeController() {
-        return "Job Portal";
+        return "Job Portal"+ UserRole.ROLE_JOB_SEEKER;
     }
 }
