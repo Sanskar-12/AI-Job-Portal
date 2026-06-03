7a011fd8ba9254d7320f1d9d83e91410fb65c900
Sanskar-12 <sanskarv2004@gmail.com>
2026-06-03 16:32:36 +0530

    Feature: Created First router in User Service

---
+++ service/job-portal-user-service/src/main/java/com/sanskar/job/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.sanskar.job.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping
+    public String HomeController() {
+        return "Job Portal";
+    }
+}
